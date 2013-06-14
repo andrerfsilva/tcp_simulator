a70bbd773c30a49e7fe39d0994338e8ed039976c
Andr Ramos <andrerfsilva@gmail.com>
2013-06-14 16:14:29 -0300

    Cálculo do IC e inclusão de alguns módulos do SSJ e Colt

--- src/br/ufrj/ad/simulator/Estimador.java
+++ src/br/ufrj/ad/simulator/Estimador.java
@@ -1,5 +1,7 @@
 package br.ufrj.ad.simulator;
 
+import umontreal.iro.lecuyer.probdist.StudentDist;
+
 /**
  * Essa classe será responsável por receber um conjunto de amostras e calcular
  * medidas estatísticas como média, variância, intervalo de confiança (IC), etc
@@ -7,6 +9,8 @@ package br.ufrj.ad.simulator;
  * Estimador. Esse Estimador otimiza o uso de memória estimando os parâmetros de
  * interesse de forma incremental, ou seja, sem a necessidade de armazenar todas
  * as amotras colhidas na memória.
+ * 
+ * Importante: as amostras devem ser independentes e identicamente distribuídas.
  * 
  * @author André Ramos
  * 
@@ -25,12 +29,16 @@ public class Estimador {
 
 	/**
 	 * Informa ao Estimador o valor de uma nova amostra da variável aleatória de
-	 * interesse.
+	 * interesse. Importante: amostras precisam ser independentes e
+	 * identicamente distribuídas.
 	 * 
 	 * @param amostra
 	 *            uma amostra da v.a. de interesse
 	 */
 	public void coletarAmostra(double amostra) {
+		// TODO: Para um número de armostras muito grande dá overflow! Com
+		// 100.000 amostras usando a distribuição exponencial ou a geométrica já
+		// pode ocorrer esse problema. Pensar em um algoritmo melhor.
 		somaAmostras += amostra;
 		somaQuadradoAmostras += amostra * amostra;
 		numeroAmostras++;
@@ -53,8 +61,12 @@ public class Estimador {
 	 * @return estimador da variância
 	 */
 	public double getVariancia() {
-		return (somaQuadradoAmostras / numeroAmostras)
-				- (somaAmostras * somaAmostras / ((numeroAmostras - 1) * numeroAmostras));
+		if (numeroAmostras > 1) {
+			return (somaQuadradoAmostras / numeroAmostras)
+					- (somaAmostras * somaAmostras / ((numeroAmostras - 1) * numeroAmostras));
+		} else {
+			return 0;
+		}
 	}
 
 	/**
@@ -65,6 +77,45 @@ public class Estimador {
 	 */
 	public double getDesvioPadrao() {
 		return Math.sqrt(getVariancia());
+	}
+
+	/**
+	 * Calcula U(α) o limite superior do IC para a dada confiança.
+	 * 
+	 * @param confianca
+	 *            probabilidade da média real estar entre U(α) e L(α)
+	 * @return U(α)
+	 */
+	public double getMaxICMedia(double confianca) {
+		return getMedia() + getDistanciaICMedia(confianca);
+	}
+
+	/**
+	 * Calcula L(α) o limite superior do IC para a dada confiança.
+	 * 
+	 * @param confianca
+	 *            probabilidade da média real estar entre U(α) e L(α)
+	 * @return L(α)
+	 */
+	public double getInfICMedia(double confianca) {
+		return getMedia() - getDistanciaICMedia(confianca);
+	}
+
+	/**
+	 * Calcula entre a distância da média amostral e os limites inferiores e
+	 * superiores do IC.
+	 * 
+	 * @param confianca
+	 *            probabilidade da média real estar entre U(α) e L(α)
+	 * @return distância entre a média amostral e U(α) = distância entre média
+	 *         amostal e L(α)
+	 */
+	public double getDistanciaICMedia(double confianca) {
+		double alpha = 1 - confianca;
+		double percentil = StudentDist.inverseF(numeroAmostras - 1,
+				1 - alpha / 2);
+
+		return percentil * Math.sqrt(getVariancia() / numeroAmostras);
 	}
 
 	/**
@@ -84,11 +135,9 @@ public class Estimador {
 		Random generator = new Random();
 		Estimador estimador = new Estimador();
 
-		int numberOfSamples = 10000;
+		int numberOfSamples = 1000;
 		double p = 0.1;
 
-		// TODO: Para um número de armostras muito grande ocorre overflow! Com
-		// 100.000 amostras já dá esse problema. Pensar em um algoritmo melhor.
 		for (int i = 0; i < numberOfSamples; i++) {
 			double sample = generator.nextGeometric(p);
 			estimador.coletarAmostra(sample);
@@ -102,9 +151,20 @@ public class Estimador {
 		double ciDistance = 1.645 * Math.sqrt(estimador.getVariancia()
 				/ numberOfSamples);
 
-		System.out.println("sample mean     = " + estimador.getMedia());
-		System.out.println("sample variance = " + estimador.getVariancia());
-		System.out.println("mean estimate   = " + estimador.getMedia() + " ± "
-				+ ciDistance + " (CI 90%)");
+		System.out.println("média amostral      = " + estimador.getMedia());
+		System.out.println("variância amostral  = " + estimador.getVariancia());
+		System.out.println("estimativa da média = " + estimador.getMedia() + " ± "
+				+ ciDistance + " (IC 90%) calculado com tabela");
+
+		/*
+		 * Testando se nossa função está calculando o percentil correto para 90%
+		 * de confiança.
+		 */
+		System.out
+				.println("estimativa da média = "
+						+ estimador.getMedia()
+						+ " ± "
+						+ estimador.getDistanciaICMedia(0.9)
+						+ " (IC 90%) calculado numericamente pela i.c.d.f. da t-student");
 	}
 }
